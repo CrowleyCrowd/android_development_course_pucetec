607581ce549e77922abea9ac6c17ad011adc9c39
Sebastián Páez <sebastian.paez@rappi.com>
2023-11-08 16:03:09 -0500

    [Clase 5] Fragments

---
+++ Clase5Fragments/app/src/main/java/com/example/clase5fragments/ui/profile/ProfileFragment.java
@@ -0,0 +1,31 @@
+package com.example.clase5fragments.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.clase5fragments.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
